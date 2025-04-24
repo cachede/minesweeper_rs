f5208d541331598bbcdb29e40c7ef352b97ed636
daniel <daniel.gibert@haw-hamburg.de>
2025-04-25 00:48:40 +0200

    add script for testing many different .mines files

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,40 @@
 use std::char::from_digit;
 use std::env;
 use std::fs;
+use std::path::PathBuf;
+use std::io;
+use std::io::ErrorKind;
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     if env::args().count() != 2 {
         println!("Please enter exactly one argument: The argument should point to a ms file");
-        return;
+        return Err(Box::from("Must enter exactly one ms file"));
     }
 
-    let minesweeper_string = file_to_string();
+    let full_path = env::args().nth(1).unwrap();
+
+    let minesweeper_string = fs::read_to_string(full_path.replace("\r", ""))?;
 
     let minesweeper_vec: Vec<Vec<char>> = minesweeper_string
         .lines()
         .map(|line| line.chars().collect())
         .collect();
 
-    if !check_valid_vec(&minesweeper_vec) {
-        eprintln!("Invalid map: it should be a rectangle");
-        return;
-    }
+    check_valid_vec(&minesweeper_vec)?;
 
-    let solved = solve_minesweeper(&minesweeper_vec);
+    let solved = solve_minesweeper(&minesweeper_vec)?;
 
     let result_string = convert_vec_to_string(&solved);
 
+    let mut output_path = PathBuf::new();
+    output_path.push(full_path);
+    output_path.set_extension("out");
+    fs::write(output_path, result_string)?;
+
     println!("Solved Minesweeper Map:");
-    println!("{}", result_string);
+
+    Ok(())
+
 }
 
 fn convert_vec_to_string(minesweeper_vec: &Vec<Vec<char>>) -> String {
@@ -41,40 +50,65 @@ fn convert_vec_to_string(minesweeper_vec: &Vec<Vec<char>>) -> String {
     result
 }
 
-fn file_to_string() -> String {
-    let absolute_path = env::current_dir().unwrap().display().to_string() + "\\";
-    let relative_path: String = env::args().skip(1).collect();
-    let full_path: String = absolute_path + &relative_path;
+fn check_valid_vec(minesweeper_vec: &Vec<Vec<char>>) -> Result<(), io::Error> {
 
-    match fs::read_to_string(&full_path) {
-        Ok(content) => content.trim().replace("\r", "").to_string(),
-        Err(e) => {
-            println!("Error reading file {}", e.to_string());
-            std::process::exit(1);
-        }
-    }
-}
+    let first_row = minesweeper_vec.first();
 
-fn check_valid_vec(minesweeper_vec: &Vec<Vec<char>>) -> bool {
-    let first_row_len = minesweeper_vec[0].len();
+    let line_size = match first_row {
+        Some(row) => row.len(),
+        None => 0,
+    };
 
-    for i in minesweeper_vec.iter() {
-        if i.len() != first_row_len {
-            return false;
+    for row in minesweeper_vec.iter() {
+
+        if row.len() != line_size {
+            return Err(io::Error::new(ErrorKind::InvalidData, "Invalid Minesweeper Map"));
         }
     }
 
-    true
+    Ok(())
 }
 
-pub fn solve_minesweeper(minesweeper_vec: &Vec<Vec<char>>) -> Vec<Vec<char>> {
+/*
+fn solve_minesweeper_rust(minesweeper_vec: &Vec<Vec<char>>) -> Result<Vec<Vec<char>>, io::Error> {
+
+    /*
+    let mut result: Vec<Vec<char>> = minesweeper_vec.clone();
+
+    let mut iterator = minesweeper_vec.iter();
+
+    let mut prev_line : Vec<char> = Vec::new();
+    let mut cur_line : Vec<char>;
+    let mut next_line : Vec<char> = iterator.next().unwrap().to_vec();
+
+    for row in iterator {
+
+        (prev_line, cur_line, next_line) = (cur_line, next_line, *row);
+
+
+    }
+
+
+     */
+
+    Ok(result)
+}
+
+ */
+
+pub fn solve_minesweeper(minesweeper_vec: &Vec<Vec<char>>) -> Result<Vec<Vec<char>>, io::Error> {
     let mut result: Vec<Vec<char>> = minesweeper_vec.clone();
 
     for (i, row) in minesweeper_vec.iter().enumerate() {
+
         for j in 0..row.len() {
-            if minesweeper_vec[i][j] == '*' {
+            if *minesweeper_vec.get(i).unwrap().get(j).unwrap() == '*' {
                 result[i][j] = '*';
                 continue;
+            }
+            if *minesweeper_vec.get(i).unwrap().get(j).unwrap() != ' ' && *minesweeper_vec.get(i).unwrap().get(j).unwrap() != '*' {
+
+                return Err(io::Error::new(ErrorKind::InvalidData, "Invalid character"));
             }
 
             let mut bombs: u32 = 0;
@@ -83,11 +117,15 @@ pub fn solve_minesweeper(minesweeper_vec: &Vec<Vec<char>>) -> Vec<Vec<char>> {
             bombs += check_vertical(&minesweeper_vec, i, j);
             bombs += check_diagonal(&minesweeper_vec, i, j);
 
-            result[i][j] = from_digit(bombs, 10).unwrap();
+            if bombs == 0 {
+                result[i][j] = ' ';
+            } else {
+                result[i][j] = from_digit(bombs, 10).unwrap();
+            }
         }
     }
 
-    result
+    Ok(result)
 }
 
 fn check_horizontal(minesweeper_vec: &Vec<Vec<char>>, row: usize, column: usize) -> u32 {
@@ -96,16 +134,37 @@ fn check_horizontal(minesweeper_vec: &Vec<Vec<char>>, row: usize, column: usize)
     let column_left_search_index: i32 = column as i32 - 1;
     let column_right_search_index: i32 = column as i32 + 1;
 
+    if let Some(char) = minesweeper_vec.get(row).unwrap().get(column_left_search_index as usize) {
+
+        if *char == '*' {
+            bombs += 1;
+        }
+    }
+
+    if let Some(char) = minesweeper_vec.get(row).unwrap().get(column_right_search_index as usize) {
+
+        if *char == '*' {
+            bombs += 1;
+        }
+    }
+
+    /*
     if column_left_search_index >= 0 {
+
+
+        /*
         if minesweeper_vec[row][column_left_search_index as usize] == '*' {
             bombs += 1;
         }
+        */
     }
     if column_right_search_index < minesweeper_vec[row].len() as i32 {
         if minesweeper_vec[row][column_right_search_index as usize] == '*' {
             bombs += 1;
         }
     }
+
+     */
 
     bombs
 }
@@ -115,6 +174,24 @@ fn check_vertical(minesweeper_vec: &Vec<Vec<char>>, row: usize, column: usize) -
 
     let row_up_search_index: i32 = row as i32 - 1;
     let row_down_search_index: i32 = row as i32 + 1;
+
+    if let Some(char) = minesweeper_vec.get(row_up_search_index as usize) {
+        if let Some(char) = minesweeper_vec.get(row_up_search_index as usize).unwrap().get(column) {
+            if *char == '*' {
+                bombs += 1;
+            }
+        }
+    }
+
+    if let Some(char) = minesweeper_vec.get(row_down_search_index as usize) {
+        if let Some(char) = minesweeper_vec.get(row_down_search_index as usize).unwrap().get(column) {
+            if *char == '*' {
+                bombs += 1;
+            }
+        }
+    }
+
+    /*
 
     if row_up_search_index >= 0 {
         if minesweeper_vec[row_up_search_index as usize][column] == '*' {
@@ -127,6 +204,8 @@ fn check_vertical(minesweeper_vec: &Vec<Vec<char>>, row: usize, column: usize) -
         }
     }
 
+     */
+
     bombs
 }
 
@@ -138,6 +217,39 @@ fn check_diagonal(minesweeper_vec: &Vec<Vec<char>>, row: usize, column: usize) -
     let column_left_search_index: i32 = column as i32 - 1;
     let column_right_search_index: i32 = column as i32 + 1;
 
+    if let Some(char) = minesweeper_vec.get(row_up_search_index as usize) {
+        if let Some(char) = minesweeper_vec.get(row_up_search_index as usize).unwrap().get(column_left_search_index as usize) {
+            if *char == '*' {
+                bombs += 1;
+            }
+        }
+    }
+
+    if let Some(char) = minesweeper_vec.get(row_up_search_index as usize) {
+        if let Some(char) = minesweeper_vec.get(row_up_search_index as usize).unwrap().get(column_right_search_index as usize) {
+            if *char == '*' {
+                bombs += 1;
+            }
+        }
+    }
+
+    if let Some(char) = minesweeper_vec.get(row_down_search_index as usize) {
+        if let Some(char) = minesweeper_vec.get(row_down_search_index as usize).unwrap().get(column_left_search_index as usize) {
+            if *char == '*' {
+                bombs += 1;
+            }
+        }
+    }
+
+    if let Some(char) = minesweeper_vec.get(row_down_search_index as usize) {
+        if let Some(char) = minesweeper_vec.get(row_down_search_index as usize).unwrap().get(column_right_search_index as usize) {
+            if *char == '*' {
+                bombs += 1
+            }
+        }
+    }
+
+    /*
     if row_up_search_index >= 0 && column_left_search_index >= 0 {
         if minesweeper_vec[row_up_search_index as usize][column_left_search_index as usize] == '*' {
             bombs += 1;
@@ -166,6 +278,8 @@ fn check_diagonal(minesweeper_vec: &Vec<Vec<char>>, row: usize, column: usize) -
             bombs += 1;
         }
     }
+
+     */
 
     bombs
 }
@@ -219,7 +333,10 @@ mod minesweeper_unit_tests {
     #[test]
     fn test_check_invalid_vec3() {
 
-        let test_vec : Vec<Vec<char>> = vec![vec!['.'], vec!['.', '.'], vec!['.', '.', '.']];
+        let test_vec : Vec<Vec<char>> = vec![
+            vec!['.'],
+            vec!['.', '.'],
+            vec!['.', '.', '.']];
 
         assert_false!(check_valid_vec(&test_vec));
     }
@@ -227,7 +344,10 @@ mod minesweeper_unit_tests {
     #[test]
     fn test_check_invalid_vec4() {
 
-        let test_vec : Vec<Vec<char>> = vec![vec!['.'], vec!['.'], vec!['.', '.']];
+        let test_vec : Vec<Vec<char>> = vec![
+            vec!['.'],
+            vec!['.'],
+            vec!['.', '.']];
 
         assert_false!(check_valid_vec(&test_vec));
     }
@@ -312,6 +432,50 @@ mod minesweeper_unit_tests {
         let result = solve_minesweeper(&input);
 
         assert_eq!(output, result);
+
+    }
+
+    #[test]
+    fn test_solve_minesweeper6() {
+
+        /*
+        let absolute_path = env::current_dir().unwrap().display().to_string() + "\\";
+        let relative_path: String = env::args().skip(1).collect();
+        let full_path: String = absolute_path + &relative_path;
+
+        let full_path = "C:\\Users\\danie\\OneDrive\\Desktop\\Rust\\Praktikum\\Aufgabe1\\src\\MinesweeperFiles\\ms1.mines";
+
+        let minesweeper_string = file_to_string(&full_path);
+
+        match minesweeper_string {
+            Some(minesweeper_string) => {
+
+                let minesweeper_vec: Vec<Vec<char>> = minesweeper_string
+                    .lines()
+                    .map(|line| line.chars().collect())
+                    .collect();
+
+                if !check_valid_vec(&minesweeper_vec) {
+                    eprintln!("Invalid map: it should be a rectangle");
+                    return;
+                }
+
+                let solved = solve_minesweeper(&minesweeper_vec);
+
+                let result_string = convert_vec_to_string(&solved);
+
+
+                fs::write("C:\\Users\\danie\\OneDrive\\Desktop\\Rust\\Praktikum\\Aufgabe1\\src\\Output\\test_output.txt", result_string).expect("Unable to write file");
+
+
+            },
+            None => {
+                println!("No file found at {}", &full_path);
+            }
+        }
+
+    */
+
 
     }
 
